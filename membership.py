2037947e48323ffb6b08182f9437dee44657c345
yuchenluisme <43535170+yuchenluisme@users.noreply.github.com>
2018-12-28 22:10:41 +0800

    Add files via upload

---
+++ membership.py
@@ -0,0 +1,4 @@
+database=[['yuchenlu','1234'],['yujun','1000'],['caiqin','8901'],['yaoshou','3456']]
+username=input('username:')
+password=input('password:')
+if[username,password] in database:print('agent permitted')
